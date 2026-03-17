51b35ce1e676ee99a4933757bc1aefe72ce6a8ea
Andreas Høisæter <a.hoisater@gmail.com>
2026-03-17 14:27:31 +0100

    Add files via upload

---
+++ horse_ass.py
@@ -0,0 +1,36 @@
+# Importing keyboard module
+
+import keyboard
+
+# Print welcome message
+
+print("Welcome to the horse race!\n")
+
+# ASCII lol?
+
+print("--oooo/-")
+print("  | |  ")
+print("\n")
+
+# Printing guidelines
+
+print("Press 'SPACE' for action, 'M' for control! 'ESC' for quitting.\n")
+
+# Function for speed
+
+def galopp():
+    print("-")
+
+keyboard.add_hotkey('space', galopp)
+
+# Function for control
+
+def control():
+    print(">")
+
+keyboard.add_hotkey('m', control)
+
+# To quit
+
+keyboard.wait('esc')
+
